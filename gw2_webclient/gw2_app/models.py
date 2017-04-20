37ed3315ddd8db100d035a2a7d7d27b3422f6596
Jordi <jordi@mail.org>
2017-04-20 23:58:37 +0200

    PVE models added

--- gw2_webclient/gw2_app/models.py
+++ gw2_webclient/gw2_app/models.py
@@ -54,3 +54,39 @@ class Character(models.Model):
     gender = models.CharField(max_length=10, choices=GENDER)
     level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(80)])
     guild = models.CharField(max_length=50, blank=True)
+
+class PlayerProfile(models.Model):
+    user = models.OneToOneField(User, unique=True)
+    apikey = models.CharField(max_length=100, blank=True)
+
+    def __unicode__(self):
+        return self.user.username
+
+class GameMode(models.Model):
+    game_id = models.AutoField(primary_key=True)
+    player = models.ManyToManyField(PlayerProfile)
+
+    def __unicode__(self):
+        return self.game_id
+
+class PveMode(GameMode):
+    pve_id = models.AutoField(primary_key=True)
+
+
+class PveEvent(PveMode):
+    event_boss_name = models.CharField(max_length=20, null=True)
+    event_hardcore_boss_name = models.CharField(max_length=20, null=True, blank=True)
+    event_time = models.TimeField()
+    event_description = models.TextField()
+
+    def __unicode__(self):
+        return self.event_boss_name
+
+
+class PvePersonalStory(PveMode):
+    current_story_step_name = models.CharField(max_length=30, null=True)
+    current_story_objective = models.CharField(max_length=50, null = True)
+    current_story_description = models.TextField(null=True)
+
+    def __unicode__(self):
+        return self.current_story_step_name
